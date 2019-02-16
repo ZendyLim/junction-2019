219e41ce6b8326dc81dd95aef7a4314a64b1d1e9
Zendy Lim <zendy.me@gmail.com>
2019-02-16 17:00:54 +0900

    Feature: Complete API for getting news by topic

---
+++ components/ArticleCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs6>
+      <v-img :aspect-ratio="16/9" :src="article.urlToImage"></v-img>
+    </v-flex>
+    <v-flex xs6>
+      <p>{{article.topic}}</p>
+      <span>{{article.title}}</span>
+    </v-flex>
+    <v-flex xs8>
+      <v-progress-linear value="50"></v-progress-linear>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  name: 'article-card',
+  props: ['article']
+}
+</script>
