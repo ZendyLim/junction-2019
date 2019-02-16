d5f14531b39ad20e3c9f5b649f91f4c1a1f4a27d
Tasuku Kawahara <monja.ty0414@gmail.com>
2019-02-16 14:12:00 +0900

    fix: remove unnecessary btn and validation

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-form ref="form" v-model="valid" lazy-validation>
+  <v-form>
     <v-text-field
       v-model="name"
       :counter="10"
@@ -7,15 +7,11 @@
       label="Enter your email ID"
       required
     ></v-text-field>
-
+    <!-- TODO add nameRoules later -->
     <v-text-field v-model="password" label="Type your password" required></v-text-field>
 
     <v-text-field v-model="retypePassword" label="Retype your password"></v-text-field>
-    <v-btn :disabled="!isMachPassword" color="success" @click="validate">SIGNUP</v-btn>
-
-    <v-btn color="error" @click="reset">Reset Form</v-btn>
-
-    <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
+    <v-btn :disabled="!isMachPassword" color="success">SIGNUP</v-btn>
   </v-form>
 </template>
 
