f8f900cb8e4003c67cbb220001832e1f19b2467e
Tsubasa Kondo <tsubasa.k111@gmail.com>
2019-02-16 23:58:11 +0900

    wip: begin writing hoaxy action

--- store/actions.js
+++ store/actions.js
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import axios from 'axios';
+import unirest from 'unirest';
 
 export default {
   addAuthenticatedUser(context, user) {
@@ -52,5 +53,25 @@ export default {
     articles = shuffle(articles)
 
     context.commit('addArticles', articles)
-  }
+  },
+
+  async serveHoaxy(context, topic) {
+    const info = await new Promise((resolve) => {
+        unirest.get(url)
+        .header("X-Mashape-Key", "OHJHR7IfeCmshao1sHNDuj8PtDi1p1fNjd1jsnXbs3LPozz5rN")
+        .header("X-Mashape-Host", "sun.p.mashape.com")
+        .end((res) => {
+          if (res) {
+            resolve(res.body);
+          }
+        });
+      })
+    
+    const results = await new Promise( resolve => {
+        unirest.get("https://api-hoaxy.p.rapidapi.com/articles?sort_by=relevant&use_lucene_syntax=true&query=pizzagate+AND+date_published%3A%5B2016-10-28+TO+2016-12-04%5D")
+        .header("X-RapidAPI-Key", "adbe0b85bemshff1eee1ef3cdd3cp1d4c65jsn127c9272c6fc")
+        .end(function (result) {
+        console.log(result.status, result.headers, result.body);
+        });
+  },
 }
