42f1e9eaa25eca9c87ed17c2eb83982202da3606
Tasuku Kawahara <monja.ty0414@gmail.com>
2019-02-16 13:09:23 +0900

    add: sign up form template

---
+++ pages/signup.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Enter your email ID"
+      required
+    ></v-text-field>
+
+    <v-text-field v-model="email" :rules="emailRules" label="Type your password" required></v-text-field>
+
+    <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Retype your password"
+      required
+    ></v-select>
+
+    <v-btn :disabled="!valid" color="success" @click="validate">SIGNUP</v-btn>
+
+    <v-btn color="error" @click="reset">Reset Form</v-btn>
+
+    <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
+  </v-form>
+</template>
